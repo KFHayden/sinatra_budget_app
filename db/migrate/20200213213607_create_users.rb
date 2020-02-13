0604bd4b95fb890d39b5ee9824b18648e7608225
Kevin Hayden <kevin.fhayden@gmail.com>
2020-02-13 21:50:13 +0000

    add timestamps to users table migration

--- db/migrate/20200213213607_create_users.rb
+++ db/migrate/20200213213607_create_users.rb
@@ -4,6 +4,9 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.string :password_digest
+      
+      t.timestamps null: false
+        #Automatically gives timestamps anytime I create or update a model
     end
   end
 end
