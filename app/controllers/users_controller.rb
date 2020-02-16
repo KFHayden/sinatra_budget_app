170c071eec40e5f1547d70e7987e0c33aa54e34a
Kevin Hayden <kevin.fhayden@gmail.com>
2020-02-16 03:01:28 +0000

    fill out get and post login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,23 @@ class UsersController < ApplicationController
   
   #create the session
   post '/login' do
-    
+    @user = User.find_by(email: params[:email])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user.id #actually logs the user in
+      puts session
+      redirect to "/users/#{@users.id}"
+    else
+      "Your username and/or password are invalid"
+      redirect to '/login'
+    end
   end
   
   get '/signup' do
     erb :signup
+  end
+  
+  get '/users/:id' do
+    
   end
   
 end
