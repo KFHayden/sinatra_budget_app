a30c503c3365ca863e8c86bc636af6756977ee2c
Kevin Hayden <kevin.fhayden@gmail.com>
2020-02-18 02:28:35 +0000

    rollback to previous version and remove cost from files

--- db/migrate/20200218020332_add_cost_amount_to_expense.rb
+++
@@ -1,5 +0,0 @@
-class AddCostAmountToExpense < ActiveRecord::Migration
-  def change
-    add_column :expenses, :cost, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200218020332) do
+ActiveRecord::Schema.define(version: 20200213214157) do
 
   create_table "expenses", force: :cascade do |t|
     t.string   "category"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "cost"
   end
 
   create_table "users", force: :cascade do |t|
