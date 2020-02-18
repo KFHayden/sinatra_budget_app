ba414cb938f0a49e591f51a9aabfae55a9322208
Kevin Hayden <kevin.fhayden@gmail.com>
2020-02-18 00:47:42 +0000

    create placeholders for expenses files

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,7 +2,6 @@ class ExpensesController < ApplicationController
   
   get '/expenses/new' do
     erb :'/expenses/new'
-    #render a form to create new expenses
   end
   
   post '/expenses/new' do
