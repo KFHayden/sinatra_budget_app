cd2698df011672472f7c3b2f0e1e09fd60f00258
Kevin Hayden <kevin.fhayden@gmail.com>
2020-02-18 00:38:57 +0000

    create get and post requests for new expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,3 +1,12 @@
 class ExpensesController < ApplicationController
   
+  get '/expenses/new' do
+    erb :'/expenses/new'
+    #render a form to create new expenses
+  end
+  
+  post '/expenses/new' do
+    #create new expenses
+  end
+  
 end
