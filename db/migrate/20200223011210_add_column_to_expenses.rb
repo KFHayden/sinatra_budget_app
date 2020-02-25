cfe275a39efdcc750e57c7af9d379d1a8fd300e3
Kevin Hayden <kevin.fhayden@gmail.com>
2020-02-25 02:38:46 +0000

    add default value to cost

--- db/migrate/20200223011210_add_column_to_expenses.rb
+++ db/migrate/20200223011210_add_column_to_expenses.rb
@@ -1,5 +1,5 @@
 class AddColumnToExpenses < ActiveRecord::Migration
   def change
-    add_column :expenses, :cost, :float
+    add_column :expenses, :cost, :float, default: 0.00
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,9 @@ ActiveRecord::Schema.define(version: 20200223011210) do
   create_table "expenses", force: :cascade do |t|
     t.string   "category"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.float    "cost"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.float    "cost",       default: 0.0
   end
 
   create_table "users", force: :cascade do |t|
